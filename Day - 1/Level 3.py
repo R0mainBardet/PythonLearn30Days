955b570fca49efe57ccdbaee22624b76c8f504d7
Romain Bardet <romain.bardet@live.fr>
2025-10-20 12:04:26 +0200

    day 1 level 3

---
+++ Day - 1/Level 3.py
@@ -0,0 +1,24 @@
+import math
+
+integer = 5
+Float = 3.6
+complex = 4 - 3j
+
+string = "Romain"
+boolean = True
+list = [1, 2, 3, 4, "Romain"]
+tuple = (5, 6, 7, 8, "Bardet")
+set = {9, 10, 11, 12, "France"}
+dictionary = {
+    "name" : "Romain",
+    "family name" : "Bardet",
+    "country" : "France"
+}
+
+p1 = 2
+p2 = 3
+q1 = 10
+q2 = 8
+d = math.sqrt(((p1 - q1)**2) + ((p2 - q2)**2))
+
+print(d)
