8db5666c32cce651b85e38c41e849cf931a90d57
Romain Bardet <romain.bardet@live.fr>
2025-10-22 15:54:17 +0200

    Day 4

--- Day_04/concatenate .py
+++ Day_04/concatenate .py
@@ -13,3 +13,14 @@ print(company.lower())
 print(company.capitalize())
 print(company.title())
 print(company.swapcase())
+
+print(company.strip("Coding "))
+
+sub_string = "Coding"
+print(company.index(sub_string))
+print(company.find("Coding"))
+
+print(company.replace("Coding", "Python"))
+
+print(company.split(' '))
+print("Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(', '))
